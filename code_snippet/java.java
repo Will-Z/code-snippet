681098ac574215e645895beb85112c09a0416c47
Will-Z <evilliril@gmail.com>
2017-07-23 23:25:00 +0800

    1

--- code_snippet/java.java
+++ code_snippet/java.java
@@ -1,4 +1,15 @@
 // 列表去重
+/* 
+[input]  list = {1,2,2,2,3}
+[output] list = {1,2,3}
+*/
   HashSet set = new HashSet(list);
   list.clear();
   list.addAll(set);
+
+/*
+[input] aList = {1,2,3}; bList = {3,4,5}
+[output] aList = {1,2,3,4,5}
+*/
+aList.removeAll(bList);
+aList.addAll(bList);
